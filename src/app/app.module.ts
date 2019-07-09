b3761d62a98fd1b9a0a29c70e2cbbbb439f6eb5a
Kluckmuck <kluckmucki@gmail.com>
2019-07-09 16:27:48 +0200

    removed tiles section. changed bold color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './components/header/header.component';
+import { AreasComponent } from './areas/areas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    HeaderComponent
+    HeaderComponent,
+    AreasComponent
   ],
   imports: [
     BrowserModule,
