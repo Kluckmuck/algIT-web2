02b62302869dbe7a25dd171481281301b82dcc10
maxjou <max.jourdanis@gmail.com>
2019-09-12 18:42:16 +0200

    Change to new footer with icons

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-homepage',
@@ -13,5 +16,8 @@ export class HomepageComponent implements OnInit {
   ngOnInit() {
   }
   faCoffee = faCoffee;
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarkerAlt = faMapMarkerAlt;
 
 }
